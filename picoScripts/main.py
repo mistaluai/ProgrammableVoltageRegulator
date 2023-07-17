90bb2fb1458b9fbaaf6b936d973d7c731d86825c
Luai Waleed <mistaluai@gmail.com>
2023-07-17 22:43:20 +0300

    Update main.py

--- picoScripts/main.py
+++ picoScripts/main.py
@@ -7,8 +7,8 @@ desiredVoltage =0; #value of the desired voltage
 def getDesiredVoltage(): #calculates desired voltage based on signal received from the host
     dv_reading = desiredVoltage_adc.read_u16() #the reading after the adc
     print("dv_reading: " + str(dv_reading))
-    if dv_reading >= 9102:
-        desiredVoltage = 18* dv_reading/65535;
+    if dv_reading >= 8276:
+        desiredVoltage = 18* dv_reading/57594;
         print("desired voltage: " + str(desiredVoltage))
     else: print("can't provide voltage less than 2.5v")
 #####
@@ -23,18 +23,20 @@ def sendOutputVoltage():
     #algorithm to make output voltage in binary
     outputVoltageDigital = [1,0,0,1,0]
     for i in outputVoltageDigital:
-        
+
        if isReadyForNext.value()==1:
         isSendingPin.value(1)
 
     outputVoltagePin.value(i) #feeds bit to the pin
     print(i)
-    isSendingPin.value(0)
-    utime.sleep(0.01)
+    while isReadyForNext.value() == 0:
+        isSendingPin.value(0)
+        utime.sleep(0.01)
 
 
 
 while True:
     getDesiredVoltage()
-    sendOutputVoltage()
-    utime.sleep(1)
+    #sendOutputVoltage()
+    utime.sleep(0.5)
+

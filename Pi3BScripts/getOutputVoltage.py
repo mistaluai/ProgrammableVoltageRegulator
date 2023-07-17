c9b7c480bd33806646f58112ee3a16984c735782
Luai Waleed <mistaluai@gmail.com>
2023-07-17 06:50:59 +0300

    Create getOutputVoltage.py

---
+++ Pi3BScripts/getOutputVoltage.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
+
+readerPin = 11
+isReadyForNextPin = 13
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(isReadyForNextPin,GPIO.OUT,0)
+GPIO.setup(readerPin, GPIO.IN)
+GPIO.setwarnings(False)
+
+outputVoltage = [0,0,0,0,0]
+
+for i in range(0,5):
+	GPIO.output(isReadyForNextPin,0)
+	outputVoltage[i] = GPIO.input(readerPin)
+	print(GPIO.input(readerPin))
+	GPIO.output(isReadyForNextPin,1)
+
+
+

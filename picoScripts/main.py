c3df33f9d9a0e735cbad642745168aec15382fd2
Luai Waleed <mistaluai@gmail.com>
2023-07-19 03:10:25 +0300

    Update main.py

--- picoScripts/main.py
+++ picoScripts/main.py
@@ -2,6 +2,7 @@ from machine import ADC, Pin
 import utime
 
 desiredVoltage_adc = ADC(Pin(28)) #should be coupled with 100nF or 1uF on the ADC input to ground.
+voltageChangePin = Pin(20,Pin.IN)
 desiredVoltage =0; #value of the desired voltage
 #####
 def getDesiredVoltage(): #calculates desired voltage based on signal received from the host
@@ -15,15 +16,14 @@ def getDesiredVoltage(): #calculates desired voltage based on signal received fr
 
 getterPin = Pin(22, Pin.IN)
 outputVoltagePin = Pin(21, Pin.OUT)
-outputVoltage = 0;
+outputVoltage = 7.5;
 
 def sendOutputVoltage():
     print("output voltage stream start")
     index = 0;
-    outputVoltageDigital = [0,0,0,0,0]
     #algorithm to make output voltage in binary
-    outputVoltageDigital = [1,0,1,1,0,1,1,1,1,1]
-    while index<10:
+    outputVoltageDigital = [0,0,1,1,1,0]
+    while index<len(outputVoltageDigital):
         if getterPin.value() == 1:
             outputVoltagePin.value(outputVoltageDigital[index])
             print(outputVoltagePin.value())
@@ -34,7 +34,8 @@ def sendOutputVoltage():
 
 
 while True:
-    #getDesiredVoltage()
+    if voltageChangePin.value()==1:
+        getDesiredVoltage()
     if getterPin.value() == 1:
         sendOutputVoltage()
     utime.sleep(0.1)

e3487c24199a4b349455eeca5a1603f15cf29f1c
Luai Waleed <mistaluai@gmail.com>
2023-07-19 03:36:23 +0300

    Update getOutputVoltage.py

--- Pi3BScripts/getOutputVoltage.py
+++ Pi3BScripts/getOutputVoltage.py
@@ -16,7 +16,7 @@ sleep(0.1)
 GPIO.output(getPin,0)
 outputVoltage = [0,0,0,0,0,0,0,0,0,0]
 
-for i in range(0,5):
+for i in range(0,10):
 	print(GPIO.input(readerPin))
 	outputVoltage[i] = GPIO.input(readerPin)
 	GPIO.output(getPin,1)

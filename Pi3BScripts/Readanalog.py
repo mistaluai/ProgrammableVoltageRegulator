070dac5c3ff47c27a83f47bd873743041efe5804
Luai Waleed <mistaluai@gmail.com>
2023-08-05 07:10:39 +0300

    Create Readanalog.py

---
+++ Pi3BScripts/Readanalog.py
@@ -0,0 +1,22 @@
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import busio
+import digitalio
+import board
+import utime
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
+while True:
+	print('Raw ADC Value: ', chan.value)
+	print('ADC Voltage: ' + str(chan.voltage) + 'V')
+	sleep(0.05)

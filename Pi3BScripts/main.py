78a4780df64b981832cd8a8ea78c7a07aacf41dd
Luai Waleed <mistaluai@gmail.com>
2023-08-07 01:59:38 +0300

    Update main.py

--- Pi3BScripts/main.py
+++ Pi3BScripts/main.py
@@ -93,7 +93,7 @@ class Embedded:
 
 
 	def enablePWM(self,duty_cycle,frequency):
-		pwmScript = subprocess.run(["python"," /home/proj/Documents/embproj/Pi3BScripts/pwm.py","23",str(frequency),str(duty_cycle)],stdout=subprocess.PIPE)
+		pwmScript = subprocess.run(["python","/home/proj/Documents/embproj/Pi3BScripts/pwm.py","23",str(frequency),str(duty_cycle)],stdout=subprocess.PIPE)
 		self.pwmAttributes = pwmScript.stdout.decode("utf-8")
 		print("pwm enabled")
 

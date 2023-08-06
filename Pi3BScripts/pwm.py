fade44a4d1c4dc5305a6c09bb50cc9785af74726
Luai Waleed <mistaluai@gmail.com>
2023-08-07 02:09:40 +0300

    Update pwm.py

--- Pi3BScripts/pwm.py
+++ Pi3BScripts/pwm.py
@@ -7,13 +7,14 @@ pwmPin = int(sys.argv[1])
 f = int(sys.argv[2])
 duty_cycle = float(sys.argv[3])
 
+PIO.setwarnings(False)
 #set gpio modes
 if GPIO.getmode()==-1:
 	GPIO.setmode(GPIO.BCM)
 
 GPIO.setwarnings(False)
 GPIO.setup(pwmPin,GPIO.OUT)
-
+GPIO.setwarnings(False)
 #start pwm
 pwm = GPIO.PWM(pwmPin,f)
 pwm.start(duty_cycle);

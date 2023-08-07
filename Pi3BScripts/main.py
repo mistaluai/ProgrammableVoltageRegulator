6b74201fc79b08c667316308b020110c754362d6
Luai Waleed <mistaluai@gmail.com>
2023-08-07 03:57:53 +0300

    Update main.py

--- Pi3BScripts/main.py
+++ Pi3BScripts/main.py
@@ -9,108 +9,110 @@ import sys
 import subprocess
 import matplotlib.pyplot as plt
 
-#global variables
+# global variables
 desiredVoltage = 0
-#analog variables
-inputVoltage=0
-outputVoltage=0
+# analog variables
+inputVoltage = 0
+outputVoltage = 0
 shuntVoltage = 0
 current = 0
-resistance =0
-#pid variables
-Kp=0
-Ki=0
-Kd=0
-#constants
+resistance = 0
+# pid variables
+Kp = 0
+Ki = 0
+Kd = 0
+# constants
 shunt_resistance = 10
-inputVoltage_factor = 109/20
-outputVoltage_factor = 27/7
+inputVoltage_factor = 109 / 20
+outputVoltage_factor = 27 / 7
+
 
 class Embedded:
-	#analog channels
-	CH_inputVoltage = None
-	CH_outputVoltage = None
-	CH_shuntVoltage = None
-	CH_pwmIN = None
-	def __init__(self):
-		#Analog To Digital
-		#create the spi bus
-		spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-		print("SPI Bus created")
-		# create the cs (chip select)
-		cs = digitalio.DigitalInOut(board.D5)
-		# create the mcp object
-		mcp = MCP.MCP3008(spi, cs)
-		print("MCP Object created")
-		#channels definition
-		self.CH_inputVoltage = AnalogIn(mcp, MCP.P1, MCP.P0)
-		self.CH_outputVoltage = AnalogIn(mcp, MCP.P3, MCP.P2)
-		self.CH_shuntVoltage = AnalogIn(mcp, MCP.P4, MCP.P5)
-		self.CH_pwmIN = AnalogIn(mcp,MCP.P6)
-		print("Diffrential Channels Defined")
-		#board
-		GPIO.setmode(GPIO.BCM)
+    # analog channels
+    CH_inputVoltage = None
+    CH_outputVoltage = None
+    CH_shuntVoltage = None
+    CH_pwmIN = None
 
+    def __init__(self):
+        # Analog To Digital
+        # create the spi bus
+        spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+        print("SPI Bus created")
+        # create the cs (chip select)
+        cs = digitalio.DigitalInOut(board.D5)
+        # create the mcp object
+        mcp = MCP.MCP3008(spi, cs)
+        print("MCP Object created")
+        # channels definition
+        self.CH_inputVoltage = AnalogIn(mcp, MCP.P1, MCP.P0)
+        self.CH_outputVoltage = AnalogIn(mcp, MCP.P3, MCP.P2)
+        self.CH_shuntVoltage = AnalogIn(mcp, MCP.P4, MCP.P5)
+        self.CH_pwmIN = AnalogIn(mcp, MCP.P6)
+        print("Diffrential Channels Defined")
+        # board
+        GPIO.setmode(GPIO.BCM)
 
-	
-	def getInputVoltage(self):
-		inputVoltage = inputVoltage_factor * self.CH_inputVoltage.voltage
+    def getInputVoltage(self):
+        inputVoltage = inputVoltage_factor * self.CH_inputVoltage.voltage
 
+    def getOutputVoltage(self):
+        outputVoltage = outputVoltage_factor * self.CH_outputVoltage.voltage - self.CH_shuntVoltage.voltage;
 
-	def getOutputVoltage(self):
-		outputVoltage = outputVoltage_factor * self.CH_outputVoltage.voltage - self.CH_shuntVoltage.voltage;
+    def getCurrent(self):
+        shuntVoltage = self.CH_shuntVoltage.voltage
+        current = shuntVoltage * 100;
 
-	def getCurrent(self):
-		shuntVoltage = self.CH_shuntVoltage.voltage
-		current = shuntVoltage*100;
+    def getResistance(self):
+        if current != 0:
+            resistance = outputVoltage / current
+        else:
+            resistance = 0
 
-	def getResistance(self):
-		if current != 0:
-			resistance = outputVoltage/current
-		else: resistance = 0
+    def debugAnalogInput(self):
+        print("about to call something")
+        self.getInputVoltage()
+        self.getOutputVoltage()
+        self.getCurrent()
+        self.getResistance()
+        print("Input Voltage (V): " + str(inputVoltage) + "\nOutput Voltage (V): " + str(
+            outputVoltage) + "\nShunt Voltage (V): " + str(shuntVoltage) + "\nTotal Current (mA): " + str(
+            current) + "\nTotal Resistance (Ω): " + str(resistance))
 
-	def debugAnalogInput(self):
-		print("about to call something")
-		self.getInputVoltage()
-		self.getOutputVoltage()
-		self.getCurrent()
-		self.getResistance()
-		print("Input Voltage (V): " + str(inputVoltage) + "\nOutput Voltage (V): " + str(outputVoltage) + "\nShunt Voltage (V): " + str(shuntVoltage) + "\nTotal Current (mA): " + str(current) + "\nTotal Resistance (Ω): " + str(resistance))
+    currentCycle = 0
+    currentFrequency = 0;
 
+    def pwmSignal(self, duty_cycle, frequency):
+        if self.currentFrequency != frequency or self.currentCycle != duty_cycle:
+            self.disablePWM()
+            self.enablePWM(duty_cycle, frequency)
+            print("changes done")
 
-	currentCycle =0
-	currentFrequency=0;		
-	def pwmSignal(self, duty_cycle, frequency):
-		if self.currentFrequency!=frequency or self.currentCycle!=duty_cycle:
-			self.disablePWM()
-			self.enablePWM(duty_cycle,frequency)
-			print("changes done")
+    def disablePWM(self):
+        try:
+            subprocess.call(["pkill", "-f", "pwm.py"])
+        except:
+            pass
 
-	
-	def disablePWM(self):
-	    try:
-	        subprocess.call(["pkill", "-f", "pwm.py"])
-	    except:
-	        pass
+    def enablePWM(self, duty_cycle, frequency):
+        pwmScript = subprocess.Popen(
+            ["python", "/home/proj/Documents/embproj/Pi3BScripts/pwm.py", "23", str(frequency), str(duty_cycle)])
+        self.currentCycle = duty_cycle
+        self.currentFrequency = frequency
+        print("pwm enabled")
 
-
-	def enablePWM(self,duty_cycle,frequency):
-		pwmScript = subprocess.Popen(["python","/home/proj/Documents/embproj/Pi3BScripts/pwm.py","23",str(frequency),str(duty_cycle)])
-		self.currentCycle=duty_cycle
-		self.currentFrequency=frequency
-		print("pwm enabled")
 
 if __name__ == "__main__":
-	embeddedObject = Embedded()
-	f = int(input("enter f "))
-	dc = int(input("enter dc "))
-	#embeddedObject.debugAnalogInput()
-	for i in range(100):
-		embeddedObject.pwmSignal(dc,f)
-	    value = embeddedObject.CH_pwmIN.voltage
-	    values.append(value)
-	    sleep(0.01)
-	plt.plot(values)
-	plt.show()
+    embeddedObject = Embedded()
+    f = int(input("enter f "))
+    dc = int(input("enter dc "))
+    # embeddedObject.debugAnalogInput()
+    for i in range(100):
+        embeddedObject.pwmSignal(dc, f)
+        value = embeddedObject.CH_pwmIN.voltage
+        values.append(value)
+        sleep(0.01)
+    plt.plot(values)
+    plt.show()
 
 

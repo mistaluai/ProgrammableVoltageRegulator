01199f8085b6b406e550c5c5bba1568115a5edcc
Luai Waleed <mistaluai@gmail.com>
2023-08-09 22:07:01 +0300

    .

--- App/main.py
+++ App/main.py
@@ -7,7 +7,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 import sys
 import subprocess
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from multiprocessing import Process
 import threading
 # ui and gtk
@@ -17,6 +17,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 UI_FILE = "UI.xml"
+
 
 # global variables
 
@@ -34,12 +35,11 @@ UI_FILE = "UI.xml"
 
 
 class UI:
-    
     desiredVoltage = 0
     resistance
     CycleIncrease = 0
     CycleDecrease = 0
-    
+
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file(UI_FILE)
@@ -49,25 +49,24 @@ class UI:
         self.window_label = self.builder.get_object("Controller")
         self.window.show_all()
 
-    def button_clicked(self,increase):
-    	self.step = self.builder.get_object("step")
-        CycleIncrease = float(self.step.get_text()) 
-        
-    def button_clicked(self,decrease):
+    def ibutton_clicked(self, increase):
         self.step = self.builder.get_object("step")
-        CycleIncrease = -1 * float(self.step.get_text()) 
+        self.CycleIncrease = float(self.step.get_text())
+
+    def dbutton_clicked(self, decrease):
+        self.step = self.builder.get_object("step")
+        self.CycleIncrease = -1 * float(self.step.get_text())
 
     def button_clicked(self, Apply):
         self.Entry = self.builder.get_object("Entry")
         self.Vout = float(self.Entry.get_text())
         self.Resistance = self.builder.get_object("resistance")
         self.resistance = float(self.Resistance.get_text())
-        if (self.Vout/self.resistance)>0.1:
-        	self.warninglabel = self.builder.get_object("warning")
-        	self.warninglabel.set_label("Couldn't apply voltage, current will exceed 100mA")
+        if (self.Vout / self.resistance) > 0.1:
+            self.warninglabel = self.builder.get_object("warning")
+            self.warninglabel.set_label("Couldn't apply voltage, current will exceed 100mA")
         else:
-        	self.desiredVoltage = self.Vout
-      
+            self.desiredVoltage = self.Vout
 
     def get_voltageDesired_button_value(self, v_desired):
         self.Entry = self.builder.get_object("Entry")
@@ -82,10 +81,14 @@ class UI:
 
 class Embedded:
     timestep = 0.01
-    _100ohm = dict({2.5:17, 3:22, 3.5:26, 4:31, 4.5:37, 5:44, 5.5:50})
-    _330ohm = dict({2.5:5, 3:6, 3.5:4, 4:8, 4.5:9, 5:10.2, 5.5:11.9, 6:13.3, 6.5:14.7, 7:17, 7.5:19, 8:20.5, 8.5:23, 9:26, 9.5:29, 10:31, 10.5:35, 11:39, 11.5:42, 12:45})
-    _560ohm = dict({2.5:2.6, 3:3.1, 3.5:4, 4:4.6, 4.5:5.3, 5:5.9, 5.5:7, 6:7.5, 6.5:8.3, 7:9.6, 7.5:9.6, 8:11.2, 8.5:12.2, 9:13.4, 9.5:14.7, 10:17, 10.5:19.5, 11:20.8, 11.5:23, 12:24})
-    
+    _100ohm = dict({2.5: 17, 3: 22, 3.5: 26, 4: 31, 4.5: 37, 5: 44, 5.5: 50})
+    _330ohm = dict(
+        {2.5: 5, 3: 6, 3.5: 4, 4: 8, 4.5: 9, 5: 10.2, 5.5: 11.9, 6: 13.3, 6.5: 14.7, 7: 17, 7.5: 19, 8: 20.5, 8.5: 23,
+         9: 26, 9.5: 29, 10: 31, 10.5: 35, 11: 39, 11.5: 42, 12: 45})
+    _560ohm = dict(
+        {2.5: 2.6, 3: 3.1, 3.5: 4, 4: 4.6, 4.5: 5.3, 5: 5.9, 5.5: 7, 6: 7.5, 6.5: 8.3, 7: 9.6, 7.5: 9.6, 8: 11.2,
+         8.5: 12.2, 9: 13.4, 9.5: 14.7, 10: 17, 10.5: 19.5, 11: 20.8, 11.5: 23, 12: 24})
+
     # # analog channels
 
     # CH_inputVoltage = None
@@ -104,7 +107,7 @@ class Embedded:
     # error = 0
     ui = None
 
-    def __init__(self,ui):
+    def __init__(self, ui):
         # # Analog To Digital
         # # create the spi bus
         # spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -186,19 +189,19 @@ class Embedded:
     currentFrequency = 0
 
     def setCycle(self):
-    	if ui.resistance != 330 and ui.resistance != 560 and ui.resistance != 100:
-    		if ui.resistance > 100 and ui.resistance < 330:
-    			self.dutyCycle = _100ohm[ui.desiredVoltage] if (ui.resistance < 215) else _330ohm[ui.desiredVoltage]
-    		elif ui.resistance > 330 and ui.resistance < 560:
-    			self.dutyCycle = _330ohm[ui.desiredVoltage] if (ui.resistance < 445) else _560ohm[ui.desiredVoltage]
-    	elif ui.resistance == 100:
-    		self.dutyCycle = _100ohm[ui.desiredVoltage]
-    	elif ui.resistance == 330:
-    		self.dutyCycle = _330ohm[ui.desiredVoltage]
-    	elif ui.resistance == 560:
-    		self.dutyCycle = _560ohm[ui.desiredVoltage]
-    	else:
-    		dutyCycle = 0
+        if ui.resistance != 330 and ui.resistance != 560 and ui.resistance != 100:
+            if ui.resistance > 100 and ui.resistance < 330:
+                self.dutyCycle = _100ohm[ui.desiredVoltage] if (ui.resistance < 215) else _330ohm[ui.desiredVoltage]
+            elif ui.resistance > 330 and ui.resistance < 560:
+                self.dutyCycle = _330ohm[ui.desiredVoltage] if (ui.resistance < 445) else _560ohm[ui.desiredVoltage]
+        elif ui.resistance == 100:
+            self.dutyCycle = _100ohm[ui.desiredVoltage]
+        elif ui.resistance == 330:
+            self.dutyCycle = _330ohm[ui.desiredVoltage]
+        elif ui.resistance == 560:
+            self.dutyCycle = _560ohm[ui.desiredVoltage]
+        else:
+            dutyCycle = 0
 
     def pwmSignal(self, duty_cycle, frequency):
         if self.currentFrequency != frequency or self.currentCycle != duty_cycle:

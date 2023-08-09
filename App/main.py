147fad0dd9805e2b83842c97b2b482ba3e9ed430
Luai Waleed <mistaluai@gmail.com>
2023-08-09 20:30:15 +0300

    .

--- App/main.py
+++ App/main.py
@@ -7,7 +7,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 import sys
 import subprocess
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from multiprocessing import Process
 import threading
 # ui and gtk
@@ -20,17 +20,17 @@ UI_FILE = "UI.xml"
 
 # global variables
 
-# analog variables
-inputVoltage = 0
-outputVoltage = 0
-shuntVoltage = 0
-current = 0
-resistance = 0
+# # analog variables
+# inputVoltage = 0
+# outputVoltage = 0
+# shuntVoltage = 0
+# current = 0
+# resistance = 0
 
-# constants
-shunt_resistance = 10
-inputVoltage_factor = 109 / 20
-outputVoltage_factor = 52 / 7
+# # constants
+# shunt_resistance = 10
+# inputVoltage_factor = 109 / 20
+# outputVoltage_factor = 52 / 7
 
 
 class UI:
@@ -73,40 +73,43 @@ class UI:
 
 class Embedded:
     timestep = 0.01
-    # analog channels
+    _100ohm = dict({2.5:17, 3:22, 3.5:26, 4:31, 4.5:37, 5:44, 5.5:50})
+    _330ohm = dict({2.5:5, 3:6, 3.5:4, 4:8, 4.5:9, 5:10.2, 5.5:11.9, 6:13.3, 6.5:14.7, 7:17, 7.5:19, 8:20.5, 8.5:23, 9:26, 9.5:29, 10:31, 10.5:35, 11:39, 11.5:42, 12:45})
+    _560ohm = dict({2.5:2.6, 3:3.1, 3.5:4, 4:4.6, 4.5:5.3, 5:5.9, 5.5:7, 6:7.5, 6.5:8.3, 7:9.6, 7.5:9.6, 8:11.2, 8.5:12.2, 9:13.4, 9.5:14.7, 10:17, 10.5:19.5, 11:20.8, 11.5:23, 12:24})
+    # # analog channels
 
-    CH_inputVoltage = None
-    CH_outputVoltage = None
-    CH_shuntVoltage = None
-    CH_pwmIN = None
-    # pid variables
-    Kp = 0
-    Ki = 0
-    Kd = 0
-    integrator = 0
-    maxIntegrator = 0
-    diffrentiator = 0
-    prevError = 0
-    prevMeasurment = 0
-    error = 0
+    # CH_inputVoltage = None
+    # CH_outputVoltage = None
+    # CH_shuntVoltage = None
+    # CH_pwmIN = None
+    # # pid variables
+    # Kp = 0
+    # Ki = 0
+    # Kd = 0
+    # integrator = 0
+    # maxIntegrator = 0
+    # diffrentiator = 0
+    # prevError = 0
+    # prevMeasurment = 0
+    # error = 0
     globalDutyCycle = 0
 
     def __init__(self):
-        # Analog To Digital
-        # create the spi bus
-        spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-        print("SPI Bus created")
-        # create the cs (chip select)
-        cs = digitalio.DigitalInOut(board.D5)
-        # create the mcp object
-        mcp = MCP.MCP3008(spi, cs)
-        print("MCP Object created")
-        # channels definition
-        self.CH_inputVoltage = AnalogIn(mcp, MCP.P3, MCP.P2)
-        self.CH_outputVoltage = AnalogIn(mcp, MCP.P1, MCP.P0)
-        self.CH_shuntVoltage = AnalogIn(mcp, MCP.P4, MCP.P5)
-        self.CH_pwmIN = AnalogIn(mcp, MCP.P6)
-        print("Diffrential Channels Defined")
+        # # Analog To Digital
+        # # create the spi bus
+        # spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+        # print("SPI Bus created")
+        # # create the cs (chip select)
+        # cs = digitalio.DigitalInOut(board.D5)
+        # # create the mcp object
+        # mcp = MCP.MCP3008(spi, cs)
+        # print("MCP Object created")
+        # # channels definition
+        # self.CH_inputVoltage = AnalogIn(mcp, MCP.P3, MCP.P2)
+        # self.CH_outputVoltage = AnalogIn(mcp, MCP.P1, MCP.P0)
+        # self.CH_shuntVoltage = AnalogIn(mcp, MCP.P4, MCP.P5)
+        # self.CH_pwmIN = AnalogIn(mcp, MCP.P6)
+        # print("Diffrential Channels Defined")
         # board
         GPIO.setmode(GPIO.BCM)
 
